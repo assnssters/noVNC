6be814aebb0b49b4cf78bf1161f8535f5b139938
assnssters <dinhductri18@gmail.com>
2025-07-28 23:28:35 +0700

    Create run.sh

---
+++ sh/run.sh
@@ -0,0 +1,11 @@
+mkdir ~/.vnc ; sleep 1
+echo "123456" | vncpasswd -f > ~/.vnc/passwd && chmod 600 ~/.vnc/passwd
+for i in {0..99};do vncserver -kill :$i 2>/dev/null;done
+vncserver :1
+bash ../novncv 5901 80 &
+sleep 5
+
+echo "Your Link:"
+PORT=80; echo "  https://server-$PORT-$_SANDBOX_ID.env.play.instruqt.com/vnc.html?host=your-ip&port=5901&password=123456&autoconnect=true&reconnect=true"
+echo "VNC Password:"
+echo "  123456"
