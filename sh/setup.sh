57bb91dc0523a5942face5fe87e0a8f70511ded9
assnssters <dinhductri18@gmail.com>
2025-07-28 23:10:56 +0700

    Create setup.sh

---
+++ sh/setup.sh
@@ -0,0 +1,11 @@
+# Debian Based Linux
+# TriGay Script hhh
+mkdir /pkg
+ChromeInstaller() {
+wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_138.0.7204.168-1_amd64.deb -O '/pkg/chrome-stable.deb"
+apt install /pkg/chrome-stable.deb -y }
+apt update -y
+apt install tigervnc-standalone-server xfce4 xfce4-terminal xfce4-goodies xvfb dbus-x11 -y
+clear
+[ ! -f "/usr/bin/google-chrome" ] && ChromeInstaller ; echo Chrome:OK
+clear && echo "Setup success hahaha"
