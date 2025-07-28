12bf64948969df789b524461f7ed4387b7762ac7
assnssters <dinhductri18@gmail.com>
2025-07-28 23:41:01 +0700

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -2,7 +2,7 @@
 # TriGay Script hhh
 mkdir /pkg
 ChromeInstaller() {
-wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_138.0.7204.168-1_amd64.deb -O '/pkg/chrome-stable.deb"
+wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_138.0.7204.168-1_amd64.deb -O "/pkg/chrome-stable.deb"
 apt install /pkg/chrome-stable.deb -y }
 apt update -y
 apt install tigervnc-standalone-server xfce4 xfce4-terminal xfce4-goodies xvfb dbus-x11 -y
